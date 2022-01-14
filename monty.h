cf84127179704060d0334bb596def608230f5dd5
EyaZidi19 <3805@holbertonschool.com>
2022-01-13 19:37:12 -0800

    free allocated memory

--- monty.h
+++ monty.h
@@ -36,7 +36,29 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+/**
+ * struct argu_s - opcode and value
+ * @command: opcode
+ * @value: value
+ */
+typedef struct argu_s
+{
+	char *command;
+	char *value;
+} argu_t;
+extern argu_t arguments;
+argu_t arguments;
+void get_opcode(stack_t **head, unsigned int line_number);
 void _push(stack_t **head, unsigned int line_number);
 void _print_all(stack_t **head, unsigned int line_number);
+void free_stuff(char *buffer, stack_t *head);
+void print_errors(unsigned int code, char *argv, unsigned int n_line);
+int check_digit(char *string);
 void _pint(stack_t **head, unsigned int line_number);
+void _pop(stack_t **head, unsigned int line_number);
+void _swap(stack_t **head, unsigned int line_number);
+int list_len(stack_t *head);
+void _add(stack_t **head, unsigned int line_number);
+void errors1(unsigned int code, unsigned int n_line);
+void _sub(stack_t **head, unsigned int line_number);
 #endif
